2a6055e204625136b38a6dc95be02e44dd492740
Niels Lange <info@nielslange.de>
2020-04-06 20:05:20 +0700

    #5: Add support for Neve theme

---
+++ themes/neve.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Handle custom logo link for Neve theme
+ *
+ * Neve: https://wordpress.org/themes/neve/
+
+ * @category   Plugin
+ * @package    WordPress
+ * @subpackage SMNTCS Custom Logo Link
+ * @author     Niels Lange <info@nielslange.de>, Derek Smith <derek@timbre-design.com>
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+if ( get_option( 'smntcs_custom_logo_link_url' ) ) { ?>
+	<script>
+		document.addEventListener("DOMContentLoaded", function() {
+			document.querySelector(".site-header > a").setAttribute("href", "<?php print( esc_url( get_option( 'smntcs_custom_logo_link_url' ) ) ); ?>");
+		});
+	</script>
+	<?php
+	if ( get_option( 'smntcs_custom_logo_link_target' ) ) {
+		?>
+		<script>
+			document.addEventListener("DOMContentLoaded", function() {
+				document.querySelector(".site-header > a").setAttribute("target", "_blank");
+			});
+		</script> 
+		<?php
+	} else {
+		?>
+		<script>
+			document.addEventListener("DOMContentLoaded", function() {
+				document.querySelector(".site-header > a").setAttribute("target", "_self");
+			});
+		</script> 
+		<?php
+	}
+}
