c1979a352c04ad3d54e9206b2e7cb937c32e3e83
Niels Lange <info@nielslange.de>
2020-04-06 20:03:23 +0700

    #5: Add support for Hello Elementor theme

--- README.md
+++ README.md
@@ -28,6 +28,7 @@ You can find the plugin on https://wordpress.org/plugins/smntcs-custom-logo-link
 * [Add support for Astra theme](https://github.com/nielslange/smntcs-custom-logo-link/issues/5)
 * [Add support for ColorMag theme](https://github.com/nielslange/smntcs-custom-logo-link/issues/5)
 * [Add support for GeneratePress theme](https://github.com/nielslange/smntcs-custom-logo-link/issues/5)
+* [Add support for Hello Elementor theme](https://github.com/nielslange/smntcs-custom-logo-link/issues/5)
 * Tested up to 5.3
 * Replace jQuery with Vanilla JS
 
---
+++ themes/hello-elementor.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Handle custom logo link for Hello Elementor theme
+ *
+ * ✅ Hello Elementor: https://wordpress.org/themes/hello-elementor/
+
+ * @category   Plugin
+ * @package    WordPress
+ * @subpackage SMNTCS Custom Logo Link
+ * @author     Niels Lange <info@nielslange.de>, Derek Smith <derek@timbre-design.com>
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+if ( get_option( 'smntcs_custom_logo_link_url' ) ) { ?>
+	<script>
+		document.addEventListener("DOMContentLoaded", function() {
+			document.querySelector(".site-header > a").setAttribute("href", "<?php print( esc_url( get_option( 'smntcs_custom_logo_link_url' ) ) ); ?>");
+		});
+	</script>
+	<?php
+	if ( get_option( 'smntcs_custom_logo_link_target' ) ) {
+		?>
+		<script>
+			document.addEventListener("DOMContentLoaded", function() {
+				document.querySelector(".site-header > a").setAttribute("target", "_blank");
+			});
+		</script> 
+		<?php
+	} else {
+		?>
+		<script>
+			document.addEventListener("DOMContentLoaded", function() {
+				document.querySelector(".site-header > a").setAttribute("target", "_self");
+			});
+		</script> 
+		<?php
+	}
+}
