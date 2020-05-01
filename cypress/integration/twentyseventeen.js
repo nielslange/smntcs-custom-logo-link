b8468ddc0f574095bb6821ec7904d94f4cab07f9
Niels Lange <info@nielslange.de>
2020-05-01 17:42:34 +0700

    #13: Add link to logo for Twenty Seventeen theme

---
+++ cypress/integration/twentyseventeen.js
@@ -0,0 +1,19 @@
+describe('Twenty Seventeen', () => {
+
+	before(function () {
+		cy.login();
+	});
+
+	it('can ensure the Twenty Seventeen theme is activated', () => {
+		cy.checkThemeActivation('twentyseventeen');
+	});
+	
+	it('can ensure the site title shows the custom link', () => {
+		cy.checkSiteTitleLink('.site-title a');
+	});
+	
+	it('can ensure the site logo shows the custom link', () => {
+		cy.checkSiteTitleLink('a.custom-logo-link');
+	});
+	
+});
